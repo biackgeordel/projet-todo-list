693d8a050cd6a4383537393daa3c7cfdf6927ba2
Biakabakana odilon <biackgeordel@gmail.com>
2022-07-07 12:48:57 +0200

    ajout de la fonction editer

--- src/features/app.js
+++ src/features/app.js
@@ -1,15 +1,133 @@
-import { tab, ul } from "./element.js";
-import { create, disabledveButton, activeTodo } from "./func.js";
-console.log(tab);
-console.log(ul);
+const tab = [
+  {
+    test: "odilon",
+    done: false,
+    edit: true,
+  },
+  {
+    test: "sohan",
+    done: true,
+    edit: false,
+  },
+];
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form>input");
 
-//tableau  contenant les elements li
-const dispatchElement = tab.map((element) => {
-  return create(element);
+//afficher les todos
+const displayTodo = () => {
+  const tabTodo = tab.map((element, index) => {
+    if (element.edit) {
+      return createEditTodo(element, index);
+    } else {
+      return create(element, index);
+    }
+  });
+  ul.innerHTML = "";
+  ul.append(...tabTodo);
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  addTodoList(tab, value);
+  displayTodo();
+  input.value = "";
 });
+//function pour creer un element li
+function create(element, index) {
+  const li = document.createElement("li");
+  const buttonDelete = document.createElement("button");
+  const buttonEditer = document.createElement("button");
+  //ajout des class sur le buttonDelete
+  buttonDelete.classList.add("btn", "btn-primary");
+  buttonEditer.classList.add("btn", "btn-primary");
+  //ajout des class sur le buttonEditer
+  buttonDelete.type = "button";
+  buttonDelete.innerText = "Suprimer";
+  buttonEditer.innerText = "Editer";
+  li.innerHTML = `<span class="todo ${element.done ? "done" : ""}"></span>
+      <p>${element.test}</p>`;
+  //ajout du buttonDelete et du buttonEditer dans l'element li
+  li.append(buttonEditer, buttonDelete);
 
-console.log(...dispatchElement);
-ul.append(...dispatchElement);
-disabledveButton();
-//event pour activer et désactiver le span todo
-ul.addEventListener("click", activeTodo);
+  li.addEventListener("click", (event) => {
+    if (event.target.classList.contains("todo")) {
+      toggleTodo(index);
+    }
+  });
+  //suppression d'element li
+  buttonDelete.addEventListener("click", (e) => {
+    e.stopPropagation();
+    deleteTodo(index);
+  });
+  buttonEditer.addEventListener("click", (e) => {
+    e.stopPropagation();
+    editer(index);
+  });
+
+  return li;
+}
+const createEditTodo = (element, index) => {
+  const li = document.createElement("li");
+  const buttonCancel = document.createElement("button");
+  const input = document.createElement("input");
+  const buttonSave = document.createElement("button");
+
+  buttonCancel.classList.add("btn", "btn-primary");
+  buttonSave.classList.add("btn", "btn-primary");
+  buttonCancel.innerText = "Cancel";
+  buttonSave.innerText = "Save";
+  input.type = "text";
+  input.value = element.test;
+  li.append(input, buttonCancel, buttonSave);
+
+  buttonSave.addEventListener("click", (e) => {
+    e.stopPropagation();
+    save(index, input);
+  });
+  buttonCancel.addEventListener("click", (e) => {
+    e.stopPropagation();
+    cancel(index);
+  });
+
+  return li;
+};
+displayTodo();
+//ajouter des todo
+function addTodoList(tab, test) {
+  if (test) {
+    tab.push({
+      test,
+      done: false,
+      edit: false,
+    });
+  }
+}
+function cancel(index) {
+  tab[index].edit = false;
+  displayTodo();
+}
+
+//supprimer le todo
+function deleteTodo(index) {
+  console.log("enfant");
+  tab.splice(index, 1);
+  displayTodo();
+}
+function save(index, input) {
+  console.log(input.value);
+  tab[index].test = input.value;
+  editer(index);
+}
+function toggleTodo(index) {
+  tab[index].done = !tab[index].done;
+  console.log(tab[index]);
+  displayTodo();
+}
+const editer = (index) => {
+  tab[index].edit = !tab[index].edit;
+  console.log(tab);
+
+  displayTodo();
+};
--- src/features/func.js
+++ src/features/func.js
@@ -1,11 +1,32 @@
 //function pour creer un element li
-export function create(element) {
+export function create(element, index) {
   const li = document.createElement("li");
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-primary");
+  button.type = "button";
+  button.innerText = "Suprimer";
   li.innerHTML = `<span class="todo ${element.done ? "done" : ""}"></span>
-      <p>${element.test}</p>
-      <button class="btn btn-primary">Supprimer</button>`;
+      <p>${element.test}</p>`;
+  li.append(button);
+  li.addEventListener("click", (event) => {
+    console.log(index);
+  });
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
   return li;
 }
+//ajouter des todo
+export function addTodoList(tab, test) {
+  tab.push({
+    test,
+    done: false,
+  });
+}
+//supprimer le todo
+function deleteTodo(index) {}
+/*
 //function pour activer et desactiver le bouton supprimer
 export const disabledveButton = () => {
   const span = document.querySelectorAll(`ul>li>span`);
@@ -26,3 +47,32 @@ export const activeTodo = (e) => {
   }
   disabledveButton();
 };
+//function pour recuperer les todos dans la tableau de todo
+export const displayTodoList = (tab, ul) => {
+  //tableau  contenant les elements li
+  const dispatchElement = tab.map((element) => {
+    return create(element);
+  });
+
+  console.log(...dispatchElement);
+  ul.innerHTML = "";
+  ul.append(...dispatchElement);
+};
+
+export function deleteTodoList(e, tab) {
+  const element = e.target;
+  console.log(tab);
+
+  if (element.classList.contains("btn")) {
+    const tabElement = tab.map((element) => create(element));
+    console.log(tabElement);
+    const parent = element.closest("li");
+    console.log(parent);
+    const index = tabElement.findIndex(
+      (element) => element.innerHTML === parent
+    );
+    console.log(index);
+    tab.splice(index, 1);
+    parent.remove();
+  }
+}*/
